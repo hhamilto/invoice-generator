aac0a10ae09e263fdbafeaff44e1fe7e62e2da23
Hurricane Hamilton <hhamilto@mtu.edu>
2015-05-16 10:23:04 -0400

    object to table mapping complete

--- persistence/index.js
+++ persistence/index.js
@@ -5,17 +5,64 @@ deferred = require('deferred')
 fs = require('fs')
 util = require('util')
 
-migrationService = require('./migration-service.js')
-migrationService.runMigrations().done()
-
-
 conf = require('./conf.json')
 
-conn = mysql.createConnection(conf.database)
+whereClauseFromObject = function(obj){
+	return _.map(_.pairs(obj), function(pair){
+		return mysql.escapeId(pair[0]) + '='+mysql.escape(pair[1])
+	}).join(' AND ')
+}
+migrationService = require('./migration-service.js')
+module.exports.initialize =function(){
+	var dbReady = deferred()
 
-conn.query('SHOW TABLES', function(rows){
-	_.each(rows, console.log)
-})
+	var conn = mysql.createConnection(conf.database)
+	migrationService.runMigrations().done(function(){
+		var ignoredTables = ['Migrations']
 
-done = deferred()
-module.exports.done = done.promise
+		conn.query('SHOW TABLES', function(err,rows){
+			var tables = _.without(_.map(rows,'Tables_in_HoursManagement'), 'Migrations')
+			_.each(tables, function(table){
+				module.exports[table] = {}
+				module.exports[table].create = function(newObj){
+					dfd = deferred()
+					conn.query('INSERT INTO '+table+' SET ?', newObj, function(err, result){
+						if(err)dfd.reject(err)
+						else dfd.resolve(result.insertId)
+					})
+					return dfd.promise
+				}
+				module.exports[table].get = function(obj){
+					dfd = deferred()
+					conn.query('SELECT * FROM '+table+' WHERE '+ whereClauseFromObject(obj), function(err, rows){
+						if(err)dfd.reject(err)
+						else dfd.resolve(rows)
+					})
+					return dfd.promise
+					
+				}
+				module.exports[table].set = function(newObj){
+					dfd = deferred()
+					conn.query('UPDATE '+table+' SET ?', newObj, function(err, result){
+						if(err)dfd.reject(err)
+						else dfd.resolve(result.insertId)
+					})
+					return dfd.promise
+				}
+				module.exports[table].remove = function(obj){
+					dfd = deferred()
+					var query = conn.query('DELETE FROM '+table+' WHERE '+whereClauseFromObject(obj), function(err, result){
+						console.log(query.sql)
+						if(err)dfd.reject(err)
+						else dfd.resolve()
+					})
+					return dfd.promise
+				}
+			})
+			dbReady.resolve()
+		})
+	})
+	return dbReady.promise
+}
+
+
